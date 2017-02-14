633bc4002f906fed67c7b5b868394963f866a1aa
Elihu Garrett <pmaekb@hotmail.com>
2017-02-14 02:59:04 -0600

    Update and rename moonlite.lua to xquic.lua

---
+++ xquic.lua
@@ -0,0 +1,49 @@
+local X = {}
+local ffi = require'ffi'
+local midi = require "luamidi"
+
+ffi.cdef"void Sleep(int ms);"
+
+function X.sleep(s)
+  ffi.C.Sleep(s*1000)
+end
+
+local function on(port, note, vel, channel)
+  if note ~= 0 then
+    midi.sendMessage(port, 144, note, vel, channel)
+  end
+end
+
+local function off(port, note, channel)
+  if note ~= 0 then
+    midi.sendMessage(port, 128, note, 0, channel)
+  end
+end
+
+function X.seq(a, ch)
+  local i = 1
+  local size
+  return function ()
+    while true do
+      on(1, a[i], 70, ch)
+      coroutine.yield(1 / #a)
+      off(1, a[i], ch)
+      i = i + 1
+      if i > #a then break end
+    end
+  end
+end
+
+function X.init(file)
+  while true do
+    local luna = xpcall(
+    function () return require(file) end,
+    function ()
+      print "YOU SCREWED IT!!"
+      X.sleep(1)
+    end)
+    package.loaded[file] = nil
+  end
+end
+
+return X
