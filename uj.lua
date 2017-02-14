d8371b6561bc7ab44ed5997840e7d7aece21a1bc
Elihu Garrett <pmaekb@hotmail.com>
2017-02-14 16:08:19 -0600

    Update uj.lua

--- uj.lua
+++ uj.lua
@@ -1,20 +1,16 @@
 sch = Rx.CooperativeScheduler.create()
-
+local _
 local tb = {
- 60, 60, 0, 72, 60, 60, 60, 60,
- 60, 60, 60, 60, 60, 60, 60, 60,
- 60, 60, 60, 60, 60, 60, 60, 60,
- 60, 60, 70, 60, 60, 60, 60, 60,
- 60, 60, 0, 72, 60, 60, 60, 60,
- 60, 60, 60, 60, 60, 60, 60, 60,
- 60, 60, 60, 60, 60, 60, 60, 60,
- 60, 60, 70, 60, 60, 60, 60, 60,
+  0, _, 0, 2,   0, 0, 2, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+  0, 0, 0, 0,   0, 0, _, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+  0, 0, 0, 2,   0, _, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
+  0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,   0, 0, 0, 0,
 }
 
-sch:schedule(x.seq(tb, 2), timer_resolution)
-sch:schedule(x.seq(tb, 0), timer_resolution)
+sch:schedule(x.seq(tb, 2, 60), timer_resolution)
+sch:schedule(x.seq(tb, 0, 60), timer_resolution)
 
-while sch.currentTime <= 32/16 do
+while sch.currentTime <= 64/16 do
   sch:update(timer_resolution)
   x.sleep(1/8)
 end
