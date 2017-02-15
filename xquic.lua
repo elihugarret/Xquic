9d38e399f7b92c5c0435bc77afea4cfc69931cbc
Elihu Garrett <pmaekb@hotmail.com>
2017-02-15 02:33:12 -0600

    Update xquic.lua

--- xquic.lua
+++ xquic.lua
@@ -8,29 +8,43 @@ function X.sleep(s)
   ffi.C.Sleep(s*1000)
 end
 
-local function on(port, note, vel, channel)
-  if type(note) ~= "number" then
-    midi.sendMessage(port, 144, note, vel, channel)
+local function on(port, note, vel, channel, root)
+  if note then
+    if type(note) == "number" then
+      note = note + root
+      midi.sendMessage(port, 144, note, vel, channel)
+    elseif type(note) == "table" then
+      for i = 1, #note do
+        note[i] = note[i] + root
+        midi.sendMessage(port, 144, note[i], vel, channel)
+      end
+    end
   end
 end
 
-local function off(port, note, channel)
-  if type(note) ~= "number" then
-    midi.sendMessage(port, 128, note, 0, channel)
+local function off(port, note, channel, root)
+  if note then
+    if type(note) == "number" then
+      note = note + root
+      midi.sendMessage(port, 128, note, 0, channel)
+    elseif  type(note) == "table" then
+      for j = 1, #note do
+        midi.sendMessage(port, 128, note[j], 0, channel)
+      end
+    end
   end
 end
 
 function X.seq(a, ch, root)
   local i = 1
-  local size
+  local size = #a
   return function ()
-    local note = a[i] + root
     while true do
-      on(1, a[i], 70, ch)
-      coroutine.yield(1 / #a)
-      off(1, a[i], ch)
+      on(1, a[i], 70, ch, root)
+      coroutine.yield()
+      off(1, a[i], ch, root)
       i = i + 1
-      if i > #a then break end
+      if i > size then break end
     end
   end
 end
@@ -40,7 +54,7 @@ function X.init(file)
     local luna = xpcall(
     function () return require(file) end,
     function ()
-      print "YOU SCREWED IT!!"
+      print "something is wrong"
       X.sleep(1)
     end)
     package.loaded[file] = nil
