a2a055c3c96d7637795cdaf5d3e5bba27771571e
Elihu Garrett <pmaekb@hotmail.com>
2017-02-15 02:31:28 -0600

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,15 @@
 Rx = require'rx'
 x = require"xquic"
-
+_ = false
 timer_resolution = 1/16
+
+prog = coroutine.wrap(function () 
+  local p = {0, -5}
+  while true do
+    for i = 1, #p do
+      coroutine.yield(p[i])
+    end
+  end
+end)
 
 x.init"uj"
